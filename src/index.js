92f4b58a5d451c2a7a1781de3a3746437f6ad46c
jaymie <jredman92@gmail.com>
2023-06-26 17:52:26 -0700

    adding route paths

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StoreProvider } from "./AppState/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
          <StoreProvider>
             <App />
          </StoreProvider>
-      </BrowserRouter>
+      </HashRouter>
    </React.StrictMode>
 );
